90704933bdf64b86f936b1e35814d0c7705fe8a9
mdarnowski <s18322@pjwstk.edu.pl>
2020-11-01 23:52:01 +0100

    innit

---
+++ Cw_1/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Cw1
+{
+    public class Program
+    {
+        
+            public static async Task Main(string[] args)
+            {
+                
+                    var httpClient = new HttpClient();
+
+                    HttpResponseMessage response;    
+
+                    response = await httpClient.GetAsync(args[0]);
+                  
+                   
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var res2 = await response.Content.ReadAsStringAsync();
+                        var regex = new Regex("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:[.][a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?[.])+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?");
+
+                        
+                        
+                        MatchCollection matches = regex.Matches(res2);
+                        foreach (var m in matches)
+                        {
+                            Console.WriteLine(m);
+                        }
+
+                    }
+
+            }
+    }
+}
+    
