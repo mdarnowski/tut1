b68f1eaa23f22e7b2535d1cb13e704fb87e037b7
mdarnowski <s18322@pjwstk.edu.pl>
2020-11-06 14:50:41 +0100

    add-req

--- Cw1/Cw1/Program.cs
+++ Cw1/Cw1/Program.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.VisualBasic;
-using System;
+﻿using System;
 using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -12,29 +11,56 @@ namespace Cw1
         public static async Task Main(string[] args)
         {
 
-            var httpClient = new HttpClient();
-
-            HttpResponseMessage response;
-
-            response = await httpClient.GetAsync(args[0]);
-
-
-            if (response.IsSuccessStatusCode)
+            try
             {
-                var res2 = await response.Content.ReadAsStringAsync();
-                var regex = new Regex("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:[.][a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?[.])+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?");
-
-
-
-                MatchCollection matches = regex.Matches(res2);
-                foreach (var m in matches)
+                if (args.Length > 0)
                 {
-                    Console.WriteLine(m);
+                    var httpClient = new HttpClient();
+
+                    HttpResponseMessage response;
+                    
+                    response = await httpClient.GetAsync(args[0]);
+                    try
+                    {
+
+                        if (response.IsSuccessStatusCode)
+                        {
+                            var res2 = await response.Content.ReadAsStringAsync();
+                            var regex = new Regex("[a-zA-Z0-9]+@[a-z]+[.][a-z]+");
+
+                            var mailDiscovered = false;
+
+                            MatchCollection matches = regex.Matches(res2);
+                            foreach (var m in matches)
+                            {
+                                Console.WriteLine(m);
+                                mailDiscovered = true;
+                            }
+
+                            if (!mailDiscovered)
+                                Console.WriteLine("No email addresses found");
+                        }
+
+
+                    }
+                    catch
+                    {
+                        Console.WriteLine("err while downloading the page");
+                      
+                    }
+
+
+                    httpClient.Dispose();
+
                 }
+                else
+                    throw new ArgumentException("args");
 
             }
-
+            catch(ArgumentNullException e)
+            {
+                Console.WriteLine(e);
+            }
         }
     }
 }
-
